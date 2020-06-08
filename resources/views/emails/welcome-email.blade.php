2d32028bdc52431cb75327ac897a913426839093
Michal Pénzeš <thatmichal@gmail.com>
2020-06-08 16:31:06 +0200

    email

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to e-Books now!
+
+Dear user,<br>
+I would like to welcome you at e-Books now!, brand new online library. Thank you for registration. In case you will have any questions or issues, feel free to contact us via e-mail address penm02@vse.cz.
+
+Best regards,<br>
+e-Books now! team
+@endcomponent
